4111a14015c16c65322a5265b38d97efa8ff46c6
Robi <rxcoviello@gmail.com>
2022-05-21 17:09:56 +0200

    Mapper Scene Update

--- izanagiXR-master/Assets/Scripts/MapperController.cs
+++ izanagiXR-master/Assets/Scripts/MapperController.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class MapperController : MonoBehaviour
 {
     [SerializeField] private GameObject assetBundleMenu;
+    [SerializeField] private bool menuCheck = true;
 
     void Update()
     {
-        if(GameObject.Find("AR Space") != null)
+        if(GameObject.Find("AR Space") != null && menuCheck)
         {
             GameObject.Find("Mapping UI(Clone)").SetActive(false);
             assetBundleMenu.SetActive(true);
+            menuCheck = false;
         }
     }
 }
